dcd69fb0057cfa379520f8daf885e17e763da0f7
Mathieu KHALEM <mathieu.khalem@gmail.com>
2023-05-02 09:49:09 +0200

    add link to homepage on title

--- packages/webapp/components/Header.tsx
+++ packages/webapp/components/Header.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Profile } from "./Profile";
 import { Pacifico } from "next/font/google";
 
@@ -6,7 +7,7 @@ const pacifico = Pacifico({ weight: "400", subsets: ["latin"] });
 export const Header = () => (
   <nav className="flex items-center justify-between flex-wrap bg-purple-200 rounded-lg px-4 py-2 h-20">
     <span className={`text-2xl tracking-wider ${pacifico.className}`}>
-      Hamm
+      <Link href="/">Hamm</Link>
     </span>
     <Profile />
   </nav>
