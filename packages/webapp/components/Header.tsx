1167db5aa293d8c21f807e7867bfebe461775662
Mathieu KHALEM <mathieu.khalem@gmail.com>
2023-05-03 23:51:00 +0200

    Add border on the header

--- packages/webapp/components/Header.tsx
+++ packages/webapp/components/Header.tsx
@@ -5,7 +5,7 @@ import { Pacifico } from "next/font/google";
 const pacifico = Pacifico({ weight: "400", subsets: ["latin"] });
 
 export const Header = () => (
-  <nav className="flex items-center justify-between flex-wrap bg-purple-200 rounded-lg px-4 py-2 h-20">
+  <nav className="flex items-center justify-between flex-wrap bg-purple-200 rounded-lg px-4 py-2 h-20 ring-1 ring-inset ring-purple-300">
     <span className={`text-2xl tracking-wider ${pacifico.className}`}>
       <Link href="/">Hamm</Link>
     </span>
