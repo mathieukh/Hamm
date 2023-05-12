8c3ed709b757617324c03f5f2a3b61454c3f138f
Mathieu KHALEM <mathieu.khalem@gmail.com>
2023-05-12 15:30:02 +0200

    Add webSocketPublicClient

--- packages/webapp/lib/web3.ts
+++ packages/webapp/lib/web3.ts
@@ -4,15 +4,19 @@ import { publicProvider } from "wagmi/providers/public";
 import { infuraProvider } from "wagmi/providers/infura";
 import { InjectedConnector } from "wagmi/connectors/injected";
 
-const { chains, publicClient } = configureChains(supportedChains, [
-  publicProvider(),
-  ...(INFURA_PROVIDER_API_KEY
-    ? [infuraProvider({ apiKey: INFURA_PROVIDER_API_KEY })]
-    : []),
-]);
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  supportedChains,
+  [
+    publicProvider(),
+    ...(INFURA_PROVIDER_API_KEY
+      ? [infuraProvider({ apiKey: INFURA_PROVIDER_API_KEY })]
+      : []),
+  ]
+);
 
 export const wagmiConfig = createConfig({
   autoConnect: false,
   connectors: [new InjectedConnector({ chains })],
   publicClient,
+  webSocketPublicClient,
 });
