bd84cd5b482bd92ffc1fb68145ec4f531f905f0a
Mathieu KHALEM <mathieu.khalem@gmail.com>
2023-05-15 09:29:50 +0200

    Add title to display address from page

--- packages/webapp/features/hamm-contract/layout/AddressPageContent.tsx
+++ packages/webapp/features/hamm-contract/layout/AddressPageContent.tsx
@@ -1,10 +1,35 @@
 import { FC } from "react";
 import { PiggyBanksCounter } from "../components/PiggyBanksCounter";
 import { Address, useNetwork } from "wagmi";
-import { Divider, Heading, SimpleGrid, Stack } from "@chakra-ui/react";
+import {
+  Divider,
+  Flex,
+  Heading,
+  IconButton,
+  SimpleGrid,
+  Stack,
+} from "@chakra-ui/react";
 import { CreatePiggyBankButton } from "../components/CreatePiggyBankButton";
 import { PiggyBanksGridForChain } from "../components/PiggyBanksGridForChain";
 import { supportedChains } from "@/config";
+import { ClipboardIcon } from "@heroicons/react/20/solid";
+import { useCopyAddress } from "@/hooks";
+
+const AddressFromPage: FC<{ address: Address }> = ({ address }) => {
+  const copyAddress = useCopyAddress(address);
+  return (
+    <Flex alignItems={"center"} gap={2}>
+      <Heading size={"sm"}>{address}</Heading>
+      <IconButton
+        size={"xs"}
+        variant={"ghost"}
+        aria-label="copy-address"
+        icon={<ClipboardIcon height={15} width={15} />}
+        onClick={copyAddress}
+      />
+    </Flex>
+  );
+};
 
 export const AddressPageContent: FC<{ address: Address }> = ({ address }) => {
   const { chain } = useNetwork();
@@ -12,6 +37,7 @@ export const AddressPageContent: FC<{ address: Address }> = ({ address }) => {
     <Stack marginTop={5} gap={2}>
       <Stack gap={2} maxWidth={"max-content"}>
         <Heading size={"lg"}>Piggy banks</Heading>
+        <AddressFromPage address={address} />
         <PiggyBanksCounter address={address} />
         {chain && !chain.unsupported && (
           <CreatePiggyBankButton size={"sm"} variant={"outline"}>
