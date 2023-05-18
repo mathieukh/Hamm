ddda0f2d1cd0cbf76a17ac57a9fefbf249c6ca94
Mathieu KHALEM <mathieu.khalem@gmail.com>
2023-05-18 18:48:55 +0200

    Remove not deployed networks

--- packages/webapp/config/index.ts
+++ packages/webapp/config/index.ts
@@ -1,8 +1,6 @@
 import {
   fantom,
   fantomTestnet,
-  arbitrum,
-  arbitrumGoerli,
   hardhat,
 } from "@wagmi/chains";
 import { Chain } from "wagmi";
@@ -13,7 +11,7 @@ const activeChain: Chain = NODE_ENV === "production" ? fantom : hardhat;
 
 const supportedChains: Chain[] =
   NODE_ENV === "production"
-    ? [fantom, arbitrum]
-    : [hardhat, arbitrumGoerli, fantomTestnet];
+    ? [fantom]
+    : [hardhat, fantomTestnet];
 
 export { INFURA_PROVIDER_API_KEY, activeChain, supportedChains };
