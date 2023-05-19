7143d63055d1f5d9c7a6233da25f7c0210edbd20
Mathieu KHALEM <mathieu.khalem@gmail.com>
2023-05-19 10:17:39 +0200

    Improve wagmi configuration

--- packages/webapp/wagmi.config.ts
+++ packages/webapp/wagmi.config.ts
@@ -1,15 +1,12 @@
 import { defineConfig } from "@wagmi/cli";
-import { react } from "@wagmi/cli/plugins";
-import { abi } from "../contract/artifacts/contracts/Hamm.sol/Hamm.json";
-import { Abi } from "abitype";
+import { react, hardhat } from "@wagmi/cli/plugins";
 
 export default defineConfig({
   out: "lib/hamm.ts",
-  contracts: [
-    {
-      name: "Hamm",
-      abi: abi as Abi,
-    },
+  plugins: [
+    react(),
+    hardhat({
+      project: "../contract",
+    }),
   ],
-  plugins: [react()],
 });
