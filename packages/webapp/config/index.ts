ac7ae56faa177e91c9c49ea89954b4976e7f6f87
Mathieu KHALEM <mathieu.khalem@gmail.com>
2023-05-19 21:16:13 +0200

    Change way to parse env

--- packages/webapp/config/index.ts
+++ packages/webapp/config/index.ts
@@ -1,17 +1,12 @@
-import {
-  fantom,
-  fantomTestnet,
-  hardhat,
-} from "@wagmi/chains";
+import { fantom, fantomTestnet, hardhat } from "@wagmi/chains";
 import { Chain } from "wagmi";
 
-const { INFURA_PROVIDER_API_KEY, NODE_ENV } = process.env;
+const INFURA_PROVIDER_API_KEY = process.env.INFURA_PROVIDER_API_KEY;
+const NODE_ENV = process.env.NODE_ENV;
 
 const activeChain: Chain = NODE_ENV === "production" ? fantom : hardhat;
 
 const supportedChains: Chain[] =
-  NODE_ENV === "production"
-    ? [fantom]
-    : [hardhat, fantomTestnet];
+  NODE_ENV === "production" ? [fantom] : [hardhat, fantomTestnet];
 
 export { INFURA_PROVIDER_API_KEY, activeChain, supportedChains };
