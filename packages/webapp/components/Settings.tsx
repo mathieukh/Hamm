e0196c21791bf42574c11909ddd09f8b89cc492d
Mathieu KHALEM <mathieu.khalem@gmail.com>
2023-05-20 12:13:44 +0200

    Improve ettings display

--- packages/webapp/components/Settings.tsx
+++ packages/webapp/components/Settings.tsx
@@ -4,19 +4,40 @@ import {
   MenuList,
   IconButton,
   MenuItem,
+  Button,
 } from "@chakra-ui/react";
-import {HiOutlineCog6Tooth} from "react-icons/hi2"
+import { HiOutlineCog6Tooth } from "react-icons/hi2";
+import { IoWarningOutline } from "react-icons/io5";
 import { ToggleColorMode } from "./ToggleColorMode";
 import { ChooseChainDropdown } from "./ChooseChainDropdown";
+import { useNetwork } from "wagmi";
 
 export const Settings = () => {
-  return (
-    <Menu closeOnSelect={false}>
+  const { chain } = useNetwork();
+  const OpenMenuButton = () => {
+    if (chain?.unsupported) {
+      return (
+        <MenuButton
+          as={Button}
+          variant={"outline"}
+          colorScheme={"orange"}
+          leftIcon={<IoWarningOutline width={15} height={15} />}
+        >
+          Wrong Network
+        </MenuButton>
+      );
+    }
+    return (
       <MenuButton
         as={IconButton}
         variant={"outline"}
         icon={<HiOutlineCog6Tooth width={15} height={15} />}
       />
+    );
+  };
+  return (
+    <Menu closeOnSelect={false}>
+      <OpenMenuButton />
       <MenuList>
         <ChooseChainDropdown />
         <MenuItem>
